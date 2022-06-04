4b63c0a1d5518a914604b2de9cb2cdd0bc6939fa
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2022-06-04 13:53:59 +0100

    Update of python script

--- todayWeather.py
+++ todayWeather.py
@@ -4,61 +4,85 @@ Description: Retrieve weather information about Madeira
              Island using python (Webscraping);
 2022
 """
+from typing import List
 
 import requests
 import datetime
+import yagmail
 from time import sleep
 from os import system, name
 from bs4 import BeautifulSoup
 
-url = ['https://www.tempo.pt/calheta.htm','https://www.tempo.pt/camara-de-lobos.htm','https://www.tempo.pt/funchal.htm','https://www.tempo.pt/machico.htm','https://www.tempo.pt/ponta-do-sol.htm','https://www.tempo.pt/porto-moniz.htm','https://www.tempo.pt/ribeira-brava.htm','https://www.tempo.pt/santa-cruz_madeira-l32099.htm','https://www.tempo.pt/santana.htm','https://www.tempo.pt/sao-vicente.htm']
+url = ['https://www.tempo.pt/calheta.htm', 'https://www.tempo.pt/camara-de-lobos.htm',
+       'https://www.tempo.pt/funchal.htm', 'https://www.tempo.pt/machico.htm', 'https://www.tempo.pt/ponta-do-sol.htm',
+       'https://www.tempo.pt/porto-moniz.htm', 'https://www.tempo.pt/ribeira-brava.htm',
+       'https://www.tempo.pt/santa-cruz_madeira-l32099.htm', 'https://www.tempo.pt/santana.htm',
+       'https://www.tempo.pt/sao-vicente.htm']
 
-lugares = ["Calheta","Camara de Lobos","Funchal","Machico","Ponta de Sol","Porto Moniz","Ribeira Brava","Santa Cruz","Santana","São Vicente"]
-
+lugares: list[str] = ["Calheta", "Camara de Lobos", "Funchal", "Machico", "Ponta de Sol", "Porto Moniz",
+                      "Ribeira Brava", "Santa Cruz", "Santana", "São Vicente"]
 
 def connect(link):
-    url = link
-    result = requests.get(url)
+    result = requests.get(link)
     doc = BeautifulSoup(result.text, "html.parser")
     return doc
 
-def atualtemp():
-    tempBox = connect('https://www.tempo.pt/madeira-provincia.htm').find("ul", {"class":"ul-top-prediccion top-pred"})
-    tempPredict = tempBox.find_all("li", {"class":"li-top-prediccion"})
-    
+def display(lugar, max_temp, min_temp):
+    global output
+    output = lugar
+    MAX_SIZE = 16
+    MAX_SIZE_TEMPERATURE = 10
+    if len(lugar) <= MAX_SIZE:
+        for space in range(MAX_SIZE - len(lugar)):
+            output = output + " "
+    output = output + " Max: " + max_temp
+    output = output + " Min: " + min_temp
+    print(output)
+    output = output + "\n"
+
+
+def atualTempo():
+    tempBox = connect('https://www.tempo.pt/madeira-provincia.htm').find("ul", {"class": "ul-top-prediccion top-pred"})
+    tempPredict = tempBox.find_all("li", {"class": "li-top-prediccion"})
     time()
     text = []
-    text.append(str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))+"\n")
     for temperatura in tempPredict:
-        lugar = temperatura.find("a", {"class":"anchors"})
+        lugar = temperatura.find("a", {"class": "anchors"})
         maxTemp = temperatura.find("span", {"class": "cMax changeUnitT"})
         minTemp = temperatura.find("span", {"class": "cMin changeUnitT"})
-        print(str(lugar.string) + " Max: " + str(maxTemp.string) + " Min: " + str(minTemp.string))
-        textStr = str(lugar.string) + " Max: " + str(maxTemp.string) + " Min: " + str(minTemp.string) + "\n";
-        text.append(textStr)
+        display(str(lugar.string), str(maxTemp.string), str(minTemp.string))
+        text.append(output)
     print("")
     saveToFile(text, "weather.txt")
+    time()
 
-    
+
+
 def saveToFile(content, file):
-    
-    with open(file, 'w') as f:
-        for strings in content:
-            f.write(strings)
-            #f.write("\n")
-        print("Content saved with success")
-    
+    try:
+        with open(file, 'w') as f:
+            for strings in content:
+                f.write(strings)
+            print("Content saved with success")
+    except FileExistsError as e:
+        print("Error opening the file\n"+e)
+    except FileNotFoundError as e:
+        print("File not Found\n"+e)
+
+# cleaning console
+def clean():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
 
 def time():
     today = datetime.datetime.now()
-    print (today.strftime("%Y-%m-%d %H:%M:%S"))
+    print(today.strftime("%Y-%m-%d %H:%M:%S.%f"))
 
-
-def execute():
-    while True:
-        atualtemp()
-        sleep(300)
+if __name__ == '__main__':
+    atualTempo()
 
 
 
-execute()
