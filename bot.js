a90a6efd82bbdfdc60513ca7ea938f7d1639d58c
Pedro Rodrigues <pedro.miguel.rodrigues00@gmail.com>
2022-06-04 17:21:45 +0100

    Rock Paper Scissors

--- bot.js
+++ bot.js
@@ -170,6 +170,43 @@ client.on("messageCreate", async(message) => {
           message.channel.send("Não");
         }
       }
+      if (message.content.includes(`${config.prefix}PPT`)) {
+        let response = ["Pedra", "Papel", "Tesoura"]
+        let user_anwser = message.content.substring(5,message.content.length);
+        let sendResponse = response[Math.floor(Math.random() * response.length)]
+        message.channel.send(sendResponse);
+        if(user_anwser == "Pedra" && sendResponse == "Papel"){
+          message.channel.send("I win!!! EZ");
+        }
+        else if(user_anwser == "Pedra" && sendResponse == "Pedra"){
+          message.channel.send("...");
+        }
+        else if(user_anwser == "Pedra" && sendResponse == "Tesoura"){
+          message.channel.send(":(");
+        }
+
+        else if(user_anwser == "Papel" && sendResponse == "Pedra"){
+          message.channel.send(":(");
+        }
+        else if(user_anwser == "Papel" && sendResponse == "Papel"){
+          message.channel.send("...");
+        }
+        else if(user_anwser == "Papel" && sendResponse == "Tesoura"){
+          message.channel.send("I win!!! EZ");
+        }
+
+
+        else if(user_anwser == "Tesoura" && sendResponse == "Papel"){
+          message.channel.send(":(");
+        }
+        else if(user_anwser == "Tesoura" && sendResponse == "Pedra"){
+          message.channel.send("I win!!! EZ");
+        }
+        else if(user_anwser == "Tesoura" && sendResponse == "Tesoura"){
+          message.channel.send("...");
+        }
+
+      }
       if (message.content.startsWith(`${config.prefix}feio`)) {
       message.channel.send(":( vou me desligar");
       }
