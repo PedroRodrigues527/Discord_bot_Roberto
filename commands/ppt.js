d27b11f5366b9a1b9e6babd6d3ab1062b0c98726
PedroRodrigues527 <pedro.miguel.rodrigues00@gmail.com>
2023-11-28 21:57:29 +0000

    refactoring

--- commands/ppt.js
+++ commands/ppt.js
@@ -1,38 +1,27 @@
 async function sendMessage(message) {
-  let response = ["Pedra", "Papel", "Tesoura"]
-        let user_anwser = message.content.substring(5,message.content.length);
-        let sendResponse = response[Math.floor(Math.random() * response.length)]
-        message.channel.send(sendResponse);
-        if(user_anwser == "Pedra" && sendResponse == "Papel"){
-          message.channel.send("I win!!! EZ");
-        }
-        else if(user_anwser == "Pedra" && sendResponse == "Pedra"){
-          message.channel.send("...");
-        }
-        else if(user_anwser == "Pedra" && sendResponse == "Tesoura"){
-          message.channel.send(":(");
-        }
-
-        else if(user_anwser == "Papel" && sendResponse == "Pedra"){
-          message.channel.send(":(");
-        }
-        else if(user_anwser == "Papel" && sendResponse == "Papel"){
-          message.channel.send("...");
-        }
-        else if(user_anwser == "Papel" && sendResponse == "Tesoura"){
-          message.channel.send("I win!!! EZ");
-        }
-
-
-        else if(user_anwser == "Tesoura" && sendResponse == "Papel"){
-          message.channel.send(":(");
-        }
-        else if(user_anwser == "Tesoura" && sendResponse == "Pedra"){
-          message.channel.send("I win!!! EZ");
-        }
-        else if(user_anwser == "Tesoura" && sendResponse == "Tesoura"){
-          message.channel.send("...");
-        }
+  let response = ["Pedra", "Papel", "Tesoura"];
+  let user_anwser = message.content.substring(5, message.content.length);
+  let sendResponse = response[Math.floor(Math.random() * response.length)];
+  message.channel.send(sendResponse);
+  if (user_anwser == "Pedra" && sendResponse == "Papel") {
+    message.channel.send("I win!!! EZ");
+  } else if (user_anwser == "Pedra" && sendResponse == "Pedra") {
+    message.channel.send("...");
+  } else if (user_anwser == "Pedra" && sendResponse == "Tesoura") {
+    message.channel.send(":(");
+  } else if (user_anwser == "Papel" && sendResponse == "Pedra") {
+    message.channel.send(":(");
+  } else if (user_anwser == "Papel" && sendResponse == "Papel") {
+    message.channel.send("...");
+  } else if (user_anwser == "Papel" && sendResponse == "Tesoura") {
+    message.channel.send("I win!!! EZ");
+  } else if (user_anwser == "Tesoura" && sendResponse == "Papel") {
+    message.channel.send(":(");
+  } else if (user_anwser == "Tesoura" && sendResponse == "Pedra") {
+    message.channel.send("I win!!! EZ");
+  } else if (user_anwser == "Tesoura" && sendResponse == "Tesoura") {
+    message.channel.send("...");
+  }
 }
 
 module.exports = {
